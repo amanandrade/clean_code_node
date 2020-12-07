c7d5c74a679ba06858d837e91757f2cf6b04884c
Amanda Andrade <43591109+amanandrade@users.noreply.github.com>
2020-12-07 10:35:24 -0300

    Update userUsecase.js

--- src/usecases/userUsecase.js
+++ src/usecases/userUsecase.js
@@ -2,6 +2,9 @@ const { response } = require('express');
 const IUserRepository = require('../interfaces/repository');
 const createUserService = require('../services/createUser')
 
+//posso criar uma classe para user service. Neste caso aqui, eu to exportando o método direto.
+//posso chamar o user repository direto.
+
 module.exports = () => {
     // const { IUserRepository } = dependencies.DatabaseServices
     const createNewUser = (req, res, next) => {
