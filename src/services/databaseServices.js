bf12badb0a15c01a51af73a7105c6ac51081e4ab
Amanda Andrade <43591109+amanandrade@users.noreply.github.com>
2020-12-28 17:23:18 -0300

    WIP: implementing persistence

---
+++ src/services/databaseServices.js
@@ -0,0 +1,11 @@
+// module.exports = class DatabaseServices {
+
+//     constructor() {
+//         this.IUserRepository = null;
+//     }
+
+//     initDatabase() {
+//         return Promise.reject(new Error('not implemented'));
+//     }
+
+// };
